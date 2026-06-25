c21f8ea4a3732fdd8112a59af4e5609ece629131
Chester Luke Maligaso <123941342+Kukaas@users.noreply.github.com>
2026-06-25 08:56:16 +0800

    remove plate recognition OCR

--- src/modules/plate-recognition/dto/verify-by-url.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsUrl } from 'class-validator';
-
-export class VerifyByUrlDTO {
-  @IsUrl({ require_protocol: true, protocols: ['http', 'https'] })
-  @IsNotEmpty()
-  imageUrl!: string;
-}
